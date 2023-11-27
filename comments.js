0b964dc5d1bfd433073b1adf3208f65da854d98c
Wil Sevilla <45079501+WdSevilla@users.noreply.github.com>
2023-11-27 03:14:06 +0000

    Copilot third commit2

--- comments.js
+++ comments.js
@@ -1,12 +1,35 @@
 // Create a web server
-const http = require('http');
+// 1. When user visits the homepage, they should see a list of comments
+// 2. When user submits the form, add the comment to the list of comments
+// 3. When user submits the form, redirect to the homepage
+// 4. When user visits the homepage, they should see the updated list of comments
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const port = 3000;
+const commentsPath = path.join(__dirname, "comments.json");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+app.get("/comments", (req, res) => {
+  const comments = fs.readFileSync(commentsPath, "utf-8");
+  const commentsJSON = JSON.parse(comments);
+  res.send(commentsJSON);
 });
+
+app.post("/comments", (req, res) => {
+  const comments = fs.readFileSync(commentsPath, "utf-8");
+  const commentsJSON = JSON.parse(comments);
+  commentsJSON.push(req.body);
+  fs.writeFileSync(commentsPath, JSON.stringify(commentsJSON));
+  res.redirect("/");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
