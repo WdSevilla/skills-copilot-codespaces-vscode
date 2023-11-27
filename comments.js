366c7941156f552540f2f39979f6f65aef935a54
Wil Sevilla <45079501+WdSevilla@users.noreply.github.com>
2023-11-27 03:10:28 +0000

    Copilot third commit2

--- comments.js
+++ comments.js
@@ -1,9 +1,9 @@
 // Create a web server
 // 1. Start the server and listen on port 3000
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+// server.listen(3000, () => {
+//     console.log('Server started on port 3000');
+// });
